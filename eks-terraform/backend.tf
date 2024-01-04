eeca78b68cb3e81050d4ad69809a2d5bb8efe053
Anandm894 <anandbm1995@gmail.com>
2024-01-04 08:42:45 +0530

    eks deleted

--- eks-terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "reddit-app-96" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
-    region = "ap-southeast-2"
-  }
-}
